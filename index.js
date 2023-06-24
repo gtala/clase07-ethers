3a3617423686effb19a03e836caac0bbe50d1c29
Guillermo Tala <guille86598@gmail.com>
2023-06-24 13:30:36 +0200

    all functions

--- index.js
+++ index.js
@@ -1,18 +1,63 @@
 const { ethers } = require("ethers")
 
+const provider = ethers.providers.getDefaultProvider('sepolia')
+
+const storeContractAddress = '0x5c9a271f51e3F9854b3D079A5b731d4c865FA18D'
+
+const myPrivateKey = ''
+const signer =  new ethers.Wallet(myPrivateKey, provider)
+
+const storeContratABI = [
+    {
+        "inputs": [],
+        "name": "retrieve",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "num",
+                "type": "uint256"
+            }
+        ],
+        "name": "store",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]
+
+const blockchainReads = 'blockchainReads'
+const constractReads = 'constractReads'
+const constractWrites = 'constractWrites'
+
 const main = ()=> {
-   const op = 'balance'
+   const op = constractReads
         switch(op) {
-            case 'balance' : 
-            consultarBalance()
-            case 'contratos' : 
-            consultarBalance()
+            case 'blockchainReads': 
+                blockchainReadsFunc()
+                break;
+            case 'constractReads' : 
+                constractReadsFunc()
+                break;
+            case 'constractWrites' : 
+                constractWritesFunc()
+                break;
         }
 
 }
 
-const consultarBalance = async () => {
-    const provider = ethers.providers.getDefaultProvider('sepolia')
+const blockchainReadsFunc = async () => {
+
 
     const blockNumber = await provider.getBlockNumber()
     console.log('blockNumber', blockNumber)
@@ -21,8 +66,19 @@ const consultarBalance = async () => {
     console.log('myBalance', myBalance.toString(), ethers.utils.formatEther( myBalance.toString()) )
 }
 
-const consultarContratos = async () => {
+const constractReadsFunc = async () => {
+ const storeContract = new ethers.Contract(storeContractAddress, storeContratABI, provider)
+ const retrieveResponse = await storeContract.retrieve()
+ console.log(retrieveResponse.toString())
+}
+
+const constractWritesFunc = async () => {
     
+    const storeContract = new ethers.Contract(storeContractAddress, storeContratABI, signer)
+    const storeTx = await storeContract.store(10)
+    await storeTx.wait()
+    console.log('done!')
+
 }
 
 
